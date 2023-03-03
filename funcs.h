ad60ac87600ca22536dffcf293ce17c9be830234
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2023-03-02 19:12:51 -0500

    added all necessary prototypes

--- funcs.h
+++ funcs.h
@@ -1,3 +1,19 @@
 #pragma once
 
 // add prototypes here
+
+#pragma once
+
+bool isDivisibleBy(int n, int d);
+
+bool isPrime(int n);
+
+int nextPrime(int n);
+
+int countPrimes(int a, int b);
+
+bool isTwinPrime(int n);
+
+int nextTwinPrime(int n);
+
+int largestTwinPrime(int a, int b);
